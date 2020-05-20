378af9633b332adbb616c9e100002202632b253b
George <shaxirrim@gmail.com>
2020-05-20 15:06:17 +0300

    901 & 230 solved

---
+++ 901. Online Stock Span - Medium/solution.cpp
@@ -0,0 +1,30 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();    
+
+class StockSpanner {
+public:
+    vector < pair < int, int > > data;
+
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int span = 1;
+        
+        int index = data.size() - 1;
+        
+        while(index >= 0 && price >= data[index].first) {
+            span  += data[index].second;
+            index -= data[index].second;
+        }
+        
+        data.push_back(make_pair(price, span));
+        return span;        
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
