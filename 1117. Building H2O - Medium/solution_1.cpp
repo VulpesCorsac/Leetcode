fdc80e28dd6914beef18b396697534c02476ea19
George <shaxirrim@gmail.com>
2020-06-02 11:57:38 +0300

    1117 solved

---
+++ 1117. Building H2O - Medium/solution_1.cpp
@@ -0,0 +1,38 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; }();
+
+class H2O {
+    mutex m_hydrogen, m_oxygen;
+    int count;
+    
+public:
+    H2O()
+        : count(0) {
+        m_hydrogen.lock();
+        m_oxygen.lock();
+        m_hydrogen.unlock();
+    }
+
+    void hydrogen(function < void() > releaseHydrogen) {
+        m_hydrogen.lock();
+
+        ++count;
+
+        // releaseHydrogen() outputs "H". Do not change or remove this line.
+        releaseHydrogen();
+
+        if (count & 1){
+            m_hydrogen.unlock();
+        } else {
+            m_oxygen.unlock();
+        }
+    }
+
+    void oxygen(function < void() >  releaseOxygen) {
+        m_oxygen.lock();
+
+        // releaseOxygen() outputs "O". Do not change or remove this line.
+        releaseOxygen();
+
+        m_hydrogen.unlock();
+    }
+};
---
+++ 1117. Building H2O - Medium/solution_2.cpp
@@ -0,0 +1,35 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; }();
+
+class H2O {
+    int cnt;
+    mutex m;
+    condition_variable cv;
+
+public:
+    H2O() 
+        : cnt(0) {
+    }
+
+    void hydrogen(function < void() > releaseHydrogen) {
+        unique_lock < mutex > l(m);
+        
+        // releaseHydrogen() outputs "H". Do not change or remove this line.
+        cv.wait(l, [=] () { return (cnt < 2) ? true : false; } );
+        releaseHydrogen();
+        
+        ++cnt;
+        cv.notify_all();
+    }
+
+    void oxygen(function < void() > releaseOxygen) {
+        unique_lock < mutex > l(m);
+        
+        // releaseOxygen() outputs "O". Do not change or remove this line.
+        cv.wait(l, [=] () { return (cnt == 2) ? true : false; } );
+        
+        releaseOxygen();
+        
+        cnt = 0;
+        cv.notify_all();
+    }
+};
