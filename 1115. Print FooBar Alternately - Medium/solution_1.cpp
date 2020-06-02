e7a54c6dd446c9d2524a7000c20476d5d28a4fe1
George <shaxirrim@gmail.com>
2020-06-02 11:05:33 +0300

    1115 solved

---
+++ 1115. Print FooBar Alternately - Medium/solution_1.cpp
@@ -0,0 +1,29 @@
+class FooBar {
+private:
+    int n;
+    mutex m1, m2;
+    
+public:
+    FooBar(int n) 
+        : n(n) {
+        m1.lock();
+        m2.lock();
+    }
+
+    void foo(function < void() > printFoo) {
+        m1.unlock();
+        for (int i = 0; i < n; i++) {
+            m1.lock();
+        	printFoo();
+            m2.unlock();
+        }
+    }
+
+    void bar(function < void() > printBar) {
+        for (int i = 0; i < n; i++) {
+            m2.lock();
+        	printBar();
+            m1.unlock();
+        }
+    }
+};
