ac0794abd3e9d1279998912272b8070c6734d5e9
George <shaxirrim@gmail.com>
2020-06-02 10:55:19 +0300

    1195 solved

---
+++ 1195. Fizz Buzz Multithreaded - Medium/solution.cpp
@@ -0,0 +1,81 @@
+class FizzBuzz {
+private:
+    int n;
+    int cnt;
+    mutex m;
+    condition_variable cv;
+    
+public:
+    FizzBuzz(int n) 
+        : n(n)
+        , cnt(1) {
+    }
+
+    // printFizz() outputs "fizz".
+    void fizz(function < void() > printFizz) {
+        while (true) {
+            unique_lock lock(m);
+            while (cnt <= n && (cnt % 3 != 0 || cnt % 5 == 0)) {
+                cv.wait(lock);
+            }
+            if (cnt > n) {
+                return;
+            }
+            
+            printFizz();
+            ++cnt;
+            cv.notify_all();
+        }
+    }
+
+    // printBuzz() outputs "buzz".
+    void buzz(function < void() > printBuzz) {
+        while (true) {
+            unique_lock lock(m);
+            while (cnt <= n && (cnt % 3 == 0 || cnt % 5 != 0)) {
+                cv.wait(lock);
+            }
+            if (cnt > n) {
+                return;
+            }
+            
+            printBuzz();
+            ++cnt;
+            cv.notify_all();
+        }
+    }
+
+    // printFizzBuzz() outputs "fizzbuzz".
+	void fizzbuzz(function < void() > printFizzBuzz) {
+        while (true) {
+            unique_lock lock(m);
+            while (cnt <= n && (cnt % 3 != 0 || cnt % 5 != 0)) {
+                cv.wait(lock);
+            }
+            if (cnt > n) {
+                return;
+            }
+            
+            printFizzBuzz();
+            ++cnt;
+            cv.notify_all();
+        }
+    }
+
+    // printNumber(x) outputs "x", where x is an integer.
+    void number(function < void(int) > printNumber) {
+        while (true) {
+            unique_lock lock(m);
+            while (cnt <= n && (cnt % 3 == 0 || cnt % 5 == 0)) {
+                cv.wait(lock);
+            }
+            if (cnt > n) {
+                return;
+            }
+            
+            printNumber(cnt);
+            ++cnt;
+            cv.notify_all();
+        }        
+    }
+};
