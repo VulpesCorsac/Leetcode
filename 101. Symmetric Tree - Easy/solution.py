3ddfd7fa33d4ca4b62bae4063f9f7603ff4394e3
George <shaxirrim@gmail.com>
2020-06-04 12:58:10 +0300

    101 solved

---
+++ 101. Symmetric Tree - Easy/solution.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def check(L,R):
+            if L and R and L.val == R.val: 
+                return check(L.left, R.right) and check(L.right, R.left)
+            return L == R
+        
+        return not root or check(root.left, root.right)
