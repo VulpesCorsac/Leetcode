2e545e49a834d475b58fc6b5063dc6930a6cf6ea
George <shaxirrim@gmail.com>
2020-06-04 11:28:58 +0300

    8 solved

---
+++ 8. String to Integer (atoi) - Medium/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def myAtoi(self, str: str) -> int:
+        str = str.strip();
+        str = re.findall('(^[\+\-0]*\d+)\D*', str)
+        
+        MAX, MIN = 2**31 - 1, -2**31
+        try:
+            res = int("".join(str))
+            return max(min(res, MAX), MIN)
+        except:
+            return 0
