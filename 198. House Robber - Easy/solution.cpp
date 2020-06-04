acd255dbf9bec26579ec742e8336722655f209df
George <shaxirrim@gmail.com>
2020-06-04 14:12:10 +0300

    198 solved

---
+++ 198. House Robber - Easy/solution.cpp
@@ -0,0 +1,20 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+        
+        int prev = 0;
+        int curr = 0;
+        
+        for (const auto& item : nums) {
+            auto tmp = prev;
+            prev = max(curr + item, prev);
+            curr = tmp;
+        }
+        
+        return prev;
+    }
+};
