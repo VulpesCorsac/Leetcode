05d6cea98814fb99d5df9cc43a8be5130edbc633
George <shaxirrim@gmail.com>
2020-06-04 09:23:30 +0300

    217 solved

---
+++ 217. Contains Duplicate - Easy/solution.cpp
@@ -0,0 +1,8 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; }();
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set < int > s(nums.begin(), nums.end());
+        return nums.size() != s.size();
+    }
+};
