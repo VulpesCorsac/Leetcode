369086f57c7eecbad98340f6a6f02a192b47390b
George <shaxirrim@gmail.com>
2020-06-04 11:36:28 +0300

    38 solved

---
+++ 38. Count and Say - Easy/solution.cpp
@@ -0,0 +1,23 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();
+class Solution {
+public:
+    string countAndSay(int n) {
+        string res = "1";
+        
+        while (--n) {
+            string cur = "";
+            for (int i = 0; i < res.length(); ++i) {
+                int cnt = 1;
+                while (i+1 < res.length() && res[i] == res[i+1]) {
+                    ++i;
+                    ++cnt;
+                }
+                
+                cur += to_string(cnt) + res[i];
+            }
+            res = cur;
+        }
+        
+        return res;
+    }
+};
