d9ad2974ca59da69f5b207f6af9804dad05f6e5b
George <shaxirrim@gmail.com>
2020-06-04 12:50:13 +0300

    234 solved

---
+++ 234. Palindrome Linked List - Easy/solution.cpp
@@ -0,0 +1,58 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head) {
+            return true;
+        }
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while (fast && fast->next && fast->next->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        
+        fast = slow->next;
+        
+        slow->next = nullptr;
+                
+        for (ListNode *left = head, *right = reverseList(fast); 
+             left && right; 
+             left = left->next, right = right->next) {
+            if (left->val != right->val) {
+                return false;
+            }
+        }
+        
+        slow->next = fast;
+        
+        return true;
+    }
+    
+private:
+    ListNode* reverseList(ListNode *head) {
+        ListNode *prev = nullptr;
+        
+        while (head) {
+            auto tmp = head->next;
+            head->next = prev;
+            prev = head;
+            head = tmp;
+        }
+        
+        return prev;
+    }
+};
