1be2e801ff6bd346f31a1ceef14b6ac6abc85fdd
George <shaxirrim@gmail.com>
2020-06-05 15:01:48 +0300

    747 solved

---
+++ 747. Largest Number At Least Twice of Others - Easy/solution.cpp
@@ -0,0 +1,25 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();
+class Solution {
+public:
+    int dominantIndex(vector < int >& nums) {
+        if (nums.size() == 1) {
+            return 0;
+        }
+        
+        int max1 = 0;
+        int max2 = 0;
+        int idx = 0;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] > max1) {
+                max2 = max1;
+                max1 = nums[i];
+                idx = i;
+            } else if (nums[i] > max2) {
+                max2 = nums[i];
+            }
+        }
+        
+        return max1 >= 2 * max2 ? idx : -1;
+    }
+};
---
+++ 747. Largest Number At Least Twice of Others - Easy/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def dominantIndex(self, nums: List[int]) -> int:
+        if len(nums) == 1: 
+            return 0
+        a, b = heapq.nlargest(2, nums)
+        return max(enumerate(nums), key=lambda x: x[1])[0] if a >= b * 2 else -1
