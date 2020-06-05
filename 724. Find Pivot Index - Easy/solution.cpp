8f04aadd2772c1bc0079d73aaf86b3ee9384fe51
George <shaxirrim@gmail.com>
2020-06-05 14:52:29 +0300

    724 solved

---
+++ 724. Find Pivot Index - Easy/solution.cpp
@@ -0,0 +1,20 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int left = 0;
+        int right = accumulate(nums.begin(), nums.end(), 0);
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            right -= nums[i];
+            
+            if (left == right) {
+                return i;
+            }
+            
+            left += nums[i];
+        }
+        
+        return -1;
+    }
+};
