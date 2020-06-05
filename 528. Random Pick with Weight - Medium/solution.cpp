38a6047167a34d001c0f50a47150e3284ce7c562
George <shaxirrim@gmail.com>
2020-06-05 10:34:41 +0300

    528 solved

---
+++ 528. Random Pick with Weight - Medium/solution.cpp
@@ -0,0 +1,20 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();
+class Solution {
+public:
+    Solution(vector < int > &w) {
+        acc.reserve(w.size());
+        for (int i : w) {
+            total += i;
+            acc.push_back(total);
+        }
+    }
+    
+    int pickIndex() {
+        int r = rand() % total;
+        auto itor = upper_bound(acc.begin(), acc.end(), r);
+        return itor - acc.begin();
+    }
+private:
+    vector < int > acc;
+    int total = 0;
+};
