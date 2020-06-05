b2424d47068d64c6b7133232d505ec5e8576f536
George <shaxirrim@gmail.com>
2020-06-05 10:21:01 +0300

    24 solved

---
+++ 24. Swap Nodes in Pairs - Medium/solution.cpp
@@ -0,0 +1,29 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (!head) {
+            return nullptr;
+        }
+        if (!head->next) {
+            return head;
+        }
+        
+        auto next = head->next;
+        head->next = swapPairs(next->next);
+        next->next = head;
+        
+        return next;
+    }
+};
