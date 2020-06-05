de72e151a17cb515dbda4d6328a1624e7293ec66
George <shaxirrim@gmail.com>
2020-06-05 13:01:39 +0300

    1089 solved

---
+++ 1089. Duplicate Zeros - Easy/solution.cpp
@@ -0,0 +1,21 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        auto zeros = count(arr.begin(), arr.end(), 0);
+        auto n = arr.size();
+        
+        for (int i = n-1; i >= 0; --i) {
+            if (i + zeros < n) {
+                arr[i+zeros] = arr[i];
+            }
+            
+            if (!arr[i]) {
+                --zeros;
+                if (i + zeros < n) {
+                    arr[i+zeros] = 0;
+                }                
+            }
+        }
+    }
+};
