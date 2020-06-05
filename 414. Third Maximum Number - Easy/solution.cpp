25a4f1d2ec0b6e891936e60b23b38f8960f501ed
George <shaxirrim@gmail.com>
2020-06-05 14:30:40 +0300

    414 solved

---
+++ 414. Third Maximum Number - Easy/solution.cpp
@@ -0,0 +1,14 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        set < int > top3;
+        for (const auto& num : nums) {
+            top3.insert(num);
+            if (top3.size() > 3) {
+                top3.erase(top3.begin());
+            }
+        }
+        return top3.size() == 3 ? *top3.begin() : *top3.rbegin();        
+    }
+};
