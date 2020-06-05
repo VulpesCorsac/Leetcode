36bec26b472300c2e3549615eb948c95c2795261
George <shaxirrim@gmail.com>
2020-06-05 11:14:55 +0300

    779 solved

---
+++ 779. K-th Symbol in Grammar - Medium/solution.cpp
@@ -0,0 +1,15 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();
+class Solution {
+public:
+    int kthGrammar(int N, int K) {
+        if (N == 1) {
+            return 0;
+        }
+        
+        if (K % 2) {
+            return (kthGrammar(N - 1, (K + 1) / 2) == 0) ? 0 : 1;
+        } else {
+            return (kthGrammar(N - 1,  K      / 2) == 0) ? 1 : 0;
+        }
+    }   
+};
