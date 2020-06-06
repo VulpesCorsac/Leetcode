9c56459dd78eafbf81c8f3b7d6b91898e5d6a217
George <shaxirrim@gmail.com>
2020-06-06 18:42:26 +0300

    150 solved

---
+++ 150. Evaluate Reverse Polish Notation - Medium/solution.cpp
@@ -0,0 +1,28 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();
+
+class Solution {
+public:
+    int evalRPN(vector < string >& tokens) {
+        unordered_map < string, function < int (int, int) > > map = {
+            { "+" , [] (int a, int b) { return a + b; } },
+            { "-" , [] (int a, int b) { return a - b; } },
+            { "*" , [] (int a, int b) { return a * b; } },
+            { "/" , [] (int a, int b) { return a / b; } }
+        };
+        
+        stack < int > st;
+        
+        for (const auto& token : tokens) {
+            if (!map.count(token)) {
+                st.push(stoi(token));
+            } else {
+                auto op1 = st.top();
+                st.pop();
+                auto op2 = st.top();
+                st.pop();
+                st.push(map[token](op2, op1));
+            }
+        }
+        return st.top();
+    }
+};
