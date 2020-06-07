8c5ef884e1ee2a60bb4234bf2114abc5bce849f3
George <shaxirrim@gmail.com>
2020-06-07 09:58:33 +0300

    225 solved

---
+++ 225. Implement Stack using Queues - Easy/solution.cpp
@@ -0,0 +1,52 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();
+
+class MyStack {
+public:
+    /** Initialize your data structure here. */
+    MyStack() {
+        
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+        q.push(x);
+        
+        int sz = q.size();
+        while (sz > 1) {
+            q.push(q.front());
+            q.pop();
+            sz--;
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        auto ans = top();
+        
+        q.pop();
+        
+        return ans;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        return q.front();
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return q.empty();
+    }
+
+private:
+    queue < int > q;
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
