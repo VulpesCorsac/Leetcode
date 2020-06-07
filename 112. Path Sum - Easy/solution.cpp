feb241c78f3bd477f28862e9ff20508bf4a4e223
George <shaxirrim@gmail.com>
2020-06-07 13:44:20 +0300

    112 solved

---
+++ 112. Path Sum - Easy/solution.cpp
@@ -0,0 +1,28 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (!root) {
+            return false;
+        }
+        
+        if (root->val == sum && !root->left && !root->right) {
+            return true;
+        }
+        
+        sum -= root->val;
+        return hasPathSum(root->left, sum) || hasPathSum(root->right, sum);
+    }
+};
