e160d959fc5e127c9445f217057e08d4c9dd061b
George <shaxirrim@gmail.com>
2020-06-08 12:55:24 +0300

    652 solved

---
+++ 652. Find Duplicate Subtrees - Medium/solution.cpp
@@ -0,0 +1,44 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector < TreeNode* > findDuplicateSubtrees(TreeNode* root) {
+        serialize(root);
+        
+        
+        vector < TreeNode* > ans;
+        for (auto [k, v] : map) {
+            if (v.size() > 1) {
+                ans.push_back(v[0]);
+            }
+        }
+        
+        return ans;
+    }
+
+private:
+    unordered_map < string, vector < TreeNode* > > map;
+
+    string serialize(TreeNode* node) {
+        if (!node) {
+            return "#";
+        }
+        
+        string s = "(" + serialize(node->left) + to_string(node->val) + serialize(node->right) + ")";
+        
+        map[s].push_back(node);
+        
+        return s;
+    }
+};
