1ecffe2822e64b99d3c720b23c231f558ef8f653
George <shaxirrim@gmail.com>
2020-06-08 13:28:14 +0300

    347 solved

---
+++ 347. Top K Frequent Elements - Medium/solution.cpp
@@ -0,0 +1,29 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();
+class Solution {
+public:
+    vector < int > topKFrequent(vector < int > & nums, int K) {
+        unordered_map < int, int > cnt;
+        for (const auto& item : nums) {
+            ++cnt[item];
+        }
+        
+        priority_queue < pair < int, int > > q;
+        
+        for (const auto& [k, v] : cnt) {
+            q.push({-v, k});
+         
+            if (q.size() > K) {
+                q.pop();
+            }
+        }
+                
+        vector < int > ans;
+        ans.reserve(q.size());
+        while (!q.empty()) {
+            ans.push_back(q.top().second);
+            q.pop();
+        }
+        
+        return ans;
+    }
+};
