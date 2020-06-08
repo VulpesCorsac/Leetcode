6a71fbb6345475a36bcf9c6ab8e875c73de57f57
George <shaxirrim@gmail.com>
2020-06-08 16:59:09 +0300

    Testing added

--- 0. A Test Dir/Test.cpp
+++ 0. A Test Dir/Test.cpp
@@ -1,26 +1,55 @@
-import math
-import numpy as np
-from scipy.stats import entropy
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;     
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();
+class Solution {
+public:
+    bool isValidSequence(TreeNode* root, vector < int >& arr, int pos = 0) {    
+        if (!root || pos >= arr.size()) {
+            return false;
+        }
+                
+        if (root->val != arr[pos]) {
+            return false;
+        }
+            
+        if (pos+1 == arr.size()) {  
+            if (!root->left && !root->right) {
+                return true;
+            } else {    
+                return false;   
+            }
+        }   
+            
+        bool result = false;    
+        if (root->left) {   
+            result |= isValidSequence(root->left, arr, pos+1);
+        }
+        if (root->right) {
+            result |= isValidSequence(root->right, arr, pos+1);
+        }
 
-class Solution:
-    def calculateEntropy1(self, input: List[int]) -> float:
-        value, counts = np.unique(input, return_counts=True)
-        return entropy(counts, base=2)
-
-    def calculateEntropy2(self, input: List[int]) -> float:
-        n = len(input)
-        x = {}
-        Entropy = 0
-        for i in range(0, n):
-            if input[i] not in x.keys():
-                x[input[i]] = 1
-            else:
-                x[input[i]] += 1
-        for key in x.keys():
-            p = x[key]/n
-            Entropy += -p*math.log2(p)
-
-        return Entropy
-
-    def calculateEntropy(self, input: List[int]) -> float:
-        return self.calculateEntropy2(input)
+        return result;
+    }
+                    
+    // Shorter code, longer runtime bacause more recursion
+    bool isValidSequence1(TreeNode* root, vector < int >& arr, int pos = 0) {
+        if (!root || root->val != arr[pos]) {
+            return false;
+        }               
+                
+        if (pos+1 == arr.size()) {  
+            return !root->left && !root->right;
+        }
+        
+        return isValidSequence(root->left, arr, pos+1) || isValidSequence(root->right, arr, pos+1);
+    }
+};
