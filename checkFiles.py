1fcb1a282e7aa3ff38ff701887fbdb3c5b3e65fb
George <shaxirrim@gmail.com>
2020-06-08 17:02:55 +0300

    Update checkFiles.py

--- checkFiles.py
+++ checkFiles.py
@@ -1,39 +1,37 @@
 import os
 
 if __name__ == '__main__':
-    main_directory = os.path.dirname(os.path.realpath(__file__))
+    main_directory = os.path.dirname(os.path.realpath(__file__)) # getting curren script directory
     
-    for root, dirs, files in os.walk(main_directory):
+    for root, dirs, files in os.walk(main_directory): # getting current subdirectories
         subdirectories = dirs
         break
-
-    for directory in subdirectories:
+    
+    for directory in subdirectories: # for each subdirectory
         if len(directory) > 5: # to ignore .git folder
-            for root, dirs, files in os.walk(os.path.join(main_directory, directory)):
+            for root, dirs, files in os.walk(os.path.join(main_directory, directory)): # getting filenames
                 current_files = files
                 break
-            
-            print(current_files)
-            
-            for file in current_files:
-                if '.' in file:
-                    file_full_path = os.path.join(main_directory, directory, file)
-                    extension = file.split('.')[1]
+                        
+            for file in current_files: # for each file
+                if '.' in file: # if file has an extension
+                    file_full_path = os.path.join(main_directory, directory, file) # getting full filepath
+                    extension = file.split('.')[1] # getting file extension
                     
                     if extension == 'db': # there is no need in preview files (Thumbs.db) in the repo
                         os.remove(file_full_path)
                     elif extension == 'md' or extension == 'cpp' or extension == 'py': # do not touch other files
-                        with open(file_full_path, 'r') as file:
+                        with open(file_full_path, 'r') as file: #read file data line
                             file_data = file.read().split('\n')
                         
-                        new_data = []
-                        for s in file_data:
-                            new_data.append(s.rstrip())
+                        new_data = [] # new data
+                        for s in file_data: # for each string in file
+                            new_data.append(s.rstrip()) # rstrip that string
                         
-                        with open(file_full_path, 'w') as file:
+                        with open(file_full_path, 'w') as file: # write new data to file
                             file.write('\n'.join(new_data))
-  
-                    if extension == 'cpp':
+                    
+                    if extension == 'cpp': # if it happens to be a cpp file
                         pass
                         
                         # insert at the beginning
