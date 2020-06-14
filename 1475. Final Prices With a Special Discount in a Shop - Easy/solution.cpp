b83c970093ea339073adf3250c353c3ebc69e745
George <shaxirrim@gmail.com>
2020-06-14 09:49:01 +0300

    cpp added

---
+++ 1475. Final Prices With a Special Discount in a Shop - Easy/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        vector < int > ans(prices.size(), 0);
+        
+        for (int i = 0; i < prices.size(); ++i) {
+            int k = -1;
+            for (int j = i+1; j < prices.size(); ++j) {
+                if (prices[j] <= prices[i]) {
+                    k = j;
+                    break;
+                }
+            }
+            
+            ans[i] = prices[i];
+            if (k > 0) {
+                ans[i] -= prices[k];
+            }
+        }
+        
+        return ans;
+    }
+};
