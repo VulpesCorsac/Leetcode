fdc83e7fd03fc6e83c202732391ebb61e19a0860
George <shaxirrim@gmail.com>
2020-06-19 14:40:14 +0300

    275 solved

---
+++ 0275. H-Index II - Medium/solution.cpp
@@ -0,0 +1,16 @@
+static auto _ = [] () { ios_base::sync_with_stdio(false); cin.tie(nullptr); return 0; } ();
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int left = 0, len = citations.size(), right = len-1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (citations[mid] >= (len-mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return len - left;
+    }
+};
